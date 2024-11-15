3b412344251c501900ada9bf40739591a4eaffb8
Shubham Kumar <skumar@monarchtractor.com>
2024-11-15 18:20:11 +0530

    updated TO-DO.py

---
+++ TO-DO.py
@@ -0,0 +1,4 @@
+# [✓] make OD evaluation work on mini-dataset
+# [] add segmmentation training to bevformer_tiny
+# [] re-train fp16 model with segmentation head
+# [] make segmentation evaluation work on mini-dataset
