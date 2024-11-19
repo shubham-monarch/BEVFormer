c17bf64d3d9337fc815a7ce511e88735c2553cd5
Shubham Kumar <skumar@monarchtractor.com>
2024-11-19 14:01:46 +0530

    tiny0-seg-det training started

--- projects/mmdet3d_plugin/datasets/pipelines/__init__.py
+++ projects/mmdet3d_plugin/datasets/pipelines/__init__.py
@@ -4,10 +4,18 @@ from .transform_3d import (
 from .formating import CustomDefaultFormatBundle3D
 from .augmentation import (CropResizeFlipImage, GlobalRotScaleTransImage)
 from .dd3d_mapper import DD3DMapper
+
+from .rasterize import RasterizeMapVectors
+
 __all__ = [
-    'PadMultiViewImage', 'NormalizeMultiviewImage', 
-    'PhotoMetricDistortionMultiViewImage', 'CustomDefaultFormatBundle3D', 'CustomCollect3D',
+    'PadMultiViewImage',
+    'NormalizeMultiviewImage',
+    'PhotoMetricDistortionMultiViewImage',
+    'CustomDefaultFormatBundle3D',
+    'CustomCollect3D',
     'RandomScaleImageMultiViewImage',
-    'CropResizeFlipImage', 'GlobalRotScaleTransImage',
+    'CropResizeFlipImage',
+    'GlobalRotScaleTransImage',
     'DD3DMapper',
+    'RasterizeMapVectors',
 ]
